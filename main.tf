80d54ecb6f06711506d7835996dd02538d5491fd
spartzames <spartzames@gmail.com>
2023-05-30 21:46:31 +0900

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region = var.region
+}
+
 resource "aws_vpc" "tfc_kthong_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
